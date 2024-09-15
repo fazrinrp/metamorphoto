05bf8ba1192d2a6d3e1f17850b32dc17d62e2d3e
fazrinrp <fazrinrp28@gmail.com>
2024-09-15 10:56:45 +0700

    091524-1100-frp: Adding new mahogany theme

--- index.php
+++ index.php
@@ -1,73 +1,80 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <link rel="icon" href="img/icon/mp-icon64.png" type="image/png" sizes="64x64" />
     <link rel="icon" href="img/icon/mp-icon128.png" type="image/png" sizes="128x128" />
 
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Metamorphoto | Katalog Undangan Digital</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
     <link rel="stylesheet" href="styles.css?v=<?php echo time(); ?>">
     <link href="https://fonts.googleapis.com/css2?family=Ovo&display=swap" rel="stylesheet" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Text&display=swap" rel="stylesheet" />
-  </head>
-  <body>
+</head>
+
+<body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#"><img src="img/logo-navbar.jpg" alt="Logo" /></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-            <a class="nav-link" href="#">Pre Wedding</a>
-            <a class="nav-link" href="#">Wedding</a>
-            <a class="nav-link" href="#">Graduation</a>
-            <a class="nav-link" href="#">Undangan Digital</a>
-          </div>
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img src="img/logo-navbar.jpg" alt="Logo" /></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" href="#">Pre Wedding</a>
+                    <a class="nav-link" href="#">Wedding</a>
+                    <a class="nav-link" href="#">Graduation</a>
+                    <a class="nav-link" href="#">Undangan Digital</a>
+                </div>
+            </div>
         </div>
-      </div>
     </nav>
 
     <section class="header">
-      <div class="page-title"></div>
+        <div class="page-title"></div>
     </section>
 
     <section class="catalog">
-      <h1>Katalog Undangan Digital</h1>
-      <div class="grid-container">
-        <div class="grid-item">
-          <img src="img/template-blue.png" alt="Blue Template" />
-          <h3>Soft Blue Sky</h3>
-          <a class="tombol" href="blue-template/index.php" target="_blank">Lihat Contoh</a>
+        <h1>Katalog Undangan Digital</h1>
+        <div class="grid-container">
+            <div class="grid-item">
+                <img src="img/template-blue.png" alt="Blue Template" />
+                <h3>Soft Blue Sky</h3>
+                <a class="tombol" href="soft-blue-sky/index.php" target="_blank">Lihat Contoh</a>
+            </div>
+            <div class="grid-item">
+                <img src="img/template-green.png" alt="Green Template" />
+                <h3>Green Leaf</h3>
+                <a class="tombol" href="green-leaf/index.php" target="_blank">Lihat Contoh</a>
+            </div>
+            <div class="grid-item">
+                <img src="img/template-mahogany.png" alt="Mahogany Template" />
+                <h3>Mahogany</h3>
+                <a class="tombol" href="mahogany/index.php">Lihat Contoh</a>
+            </div>
+            <div class="grid-item">
+                <img src="img/template-phone.png" alt="Phone Template" />
+                <h3>Coming Soon</h3>
+                <a class="tombol" href="#">Lihat Contoh</a>
+            </div>
         </div>
-        <div class="grid-item">
-          <img src="img/template-green.png" alt="Green Template" />
-          <h3>Green Leaf</h3>
-          <a class="tombol" href="green-template/index.php" target="_blank">Lihat Contoh</a>
-        </div>
-        <div class="grid-item">
-          <img src="img/template-phone.png" alt="Phone Template" />
-          <h3>Coming Soon</h3>
-          <a class="tombol" href="#">Lihat Contoh</a>
-        </div>
-        <div class="grid-item">
-          <img src="img/template-phone.png" alt="Phone Template" />
-          <h3>Coming Soon</h3>
-          <a class="tombol" href="#">Lihat Contoh</a>
-        </div>
-      </div>
     </section>
 
     <section class="how-to"></section>
 
     <footer>
-      <!-- Footer content here -->
+        <!-- Footer content here -->
     </footer>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+</body>
+
 </html>
---
+++ mahogany/sections/details.php
@@ -0,0 +1,27 @@
+<section class="details">
+    <p>Acara pernikahan akan dilaksanakan pada:</p>
+    <div class="pop-up-img card justify-content-center">
+
+        <h3>Akad Nikah</h3>
+        <h4><?php echo $tanggal_akad_nikah ?></h4>
+        <h4><?php echo $waktu_akad_nikah ?></h4>
+        <h5><?php echo $alamat_akad_nikah ?></h5>
+        <div class="maps"><a class="tombol" href="<?php echo $maps_akad_nikah ?>" target="_blank">Lihat
+                Peta</a></div>
+    </div>
+
+    <div class="pop-up-img card justify-content-center">
+
+        <h3>Resepsi</h3>
+        <h4><?php echo $tanggal_resepsi ?></h4>
+        <h4><?php echo $waktu_resepsi ?></h4>
+        <h5><?php echo $alamat_resepsi ?></h5>
+        <div class="maps"><a class="tombol" href="<?php echo $maps_resepsi ?>" target="_blank">Lihat
+                Peta</a></div>
+    </div>
+
+    <div class="pop-up-img countdown">
+        <h3>MENUJU HARI BAHAGIA</h3>
+        <div class="simply-countdown mt-5"></div>
+    </div>
+</section>
