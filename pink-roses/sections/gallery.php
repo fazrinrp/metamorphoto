0d26c21a373582e02109ae70166c50872f19e2a9
fazrinrp <fazrinrp28@gmail.com>
2024-09-17 21:50:31 +0700

    091724-2150-frp: update gallery images

--- pink-roses/sections/gallery.php
+++ pink-roses/sections/gallery.php
@@ -4,26 +4,26 @@
 
     <div class="pop-up-img gallery1 two-column-portraits">
         <div class="portrait">
-            <img src="img/gallery/gallery-portrait-1.jpeg" alt="Portrait 1" />
+            <img src="img/gallery/gallery-portrait1.jpg" alt="Portrait 1" />
         </div>
         <div class="portrait">
-            <img src="img/gallery/gallery-portrait-2.jpeg" alt="Portrait 2" />
+            <img src="img/gallery/gallery-portrait2.jpg" alt="Portrait 2" />
         </div>
     </div>
     <div class="pop-up-img gallery2 two-column-portraits">
         <div class="portrait">
-            <img src="img/gallery/gallery-portrait-3.jpeg" alt="Portrait 1" />
+            <img src="img/gallery/gallery-portrait3.jpg" alt="Portrait 1" />
         </div>
         <div class="portrait">
-            <img src="img/gallery/gallery-portrait-4.jpeg" alt="Portrait 2" />
+            <img src="img/gallery/gallery-portrait4.jpg" alt="Portrait 2" />
         </div>
     </div>
     <div class="gallery3 single-column-landscapes">
         <div class="pop-up-img landscape">
-            <img src="img/gallery/gallery-landscape-1.jpeg" alt="Landscape 1" />
+            <img src="img/gallery/gallery-landscape1.jpg" alt="Landscape 1" />
         </div>
         <div class="pop-up-img landscape">
-            <img src="img/gallery/gallery-landscape-2.jpeg" alt="Landscape 2" />
+            <img src="img/gallery/gallery-landscape2.jpg" alt="Landscape 2" />
         </div>
     </div>
 </section>
