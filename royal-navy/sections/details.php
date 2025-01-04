7fe11172b79f9a889d302599974329a56a3db1c2
fazrinrp <fazrinrp28@gmail.com>
2025-01-04 14:54:00 +0700

    fix bug

--- royal-navy/sections/details.php
+++ royal-navy/sections/details.php
@@ -14,7 +14,7 @@
                 <p><?php echo $alamat_akad_nikah ?></p>
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15837.63906133596!2d107.548293!3d-7.0784013!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68ed2ae2327e3f%3A0xefbb3112f8ddd4e4!2sLa%20Cabin%20(Cafe%20dan%20Resto)!5e0!3m2!1sen!2sid!4v1698496855354!5m2!1sen!2sid"
-                    width="85%" height="130" style="border: 0" allowfullscreen="" loading="lazy"
+                    width="80%" height="130" style="border: 0" allowfullscreen="" loading="lazy"
                     referrerpolicy="no-referrer-when-downgrade"></iframe>
                 <div class="maps"><a class="tombol" href="<?php echo $maps_akad_nikah ?>" target="_blank">Lihat
                         Peta</a></div>
@@ -36,7 +36,7 @@
                 <p><?php echo $alamat_resepsi ?></p>
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15837.63906133596!2d107.548293!3d-7.0784013!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68ed2ae2327e3f%3A0xefbb3112f8ddd4e4!2sLa%20Cabin%20(Cafe%20dan%20Resto)!5e0!3m2!1sen!2sid!4v1698496855354!5m2!1sen!2sid"
-                    width="85%" height="130" style="border: 0" allowfullscreen="" loading="lazy"
+                    width="80%" height="130" style="border: 0" allowfullscreen="" loading="lazy"
                     referrerpolicy="no-referrer-when-downgrade"></iframe>
                 <div class="maps"><a class="tombol" href="<?php echo $maps_akad_nikah ?>" target="_blank">Lihat
                         Peta</a></div>
